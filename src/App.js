5908e5f73c1a4507ec002953096bb504bd5f66ce
ytran <a11718trannhuy@gmail.com>
2023-11-06 11:59:27 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from "components/Header";
+import Sidebar from "components/Sidebar";
+import Main from "components/Main";
+
+const App = () => {
+    return (
+        <div className="w-screen h-screen flex">
+            <div className="h-full">
+                <Sidebar />
+            </div>
+            <div className="flex flex-col h-full">
+                <div className="">
+                    <Header />
+                </div>
+                <div className="">
+                    <Main />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
